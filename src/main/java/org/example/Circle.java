95a73a5ffb5193b70b200f11f9f3897f841e6e11
Sofya Odegova <sophiaodegova@mail.ru>
2022-10-26 22:24:17 +0400

    Circle area added

---
+++ src/main/java/org/example/Circle.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Circle {
+    public static void main(String [] args) {
+    double radius = 2.2;
+    double area = (Math.PI * radius * radius);
+    System.out.println("Площадь круга равна: " + area);
+}
+}
