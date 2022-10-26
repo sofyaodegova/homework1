3f37b611157b97b25dc17d229ec1dd5acb94bd90
Sofya Odegova <sophiaodegova@mail.ru>
2022-10-26 22:16:32 +0400

    Square area added

---
+++ src/main/java/org/example/Square.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Square {
+    public static void main(String [] args)
+    {
+        double lenght = 2.2;
+        double width = 2.0;
+        double area = (lenght * width);
+        System.out.println("Площадь квадрата равна: " + area);
+    }
+}
